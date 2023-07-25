806426e0dd7ce2f57eaa99227f75e8fdbbba88c0
Alessio Paccoia <alessiopcc91@gmail.com>
2023-07-25 18:35:43 +0200

    chore: fix aliases

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-echo 'alias cubbit="cd ~/cubbit"' >> ~/.bashrc
-echo 'alias cubbit-tilt="cubbit && tilt -f coordinator/Tiltfile"' >> ~/.bashrc
-echo 'alias cup="cubbit-tilt up"' >> ~/.bashrc
-echo 'alias cdown="cubbit-tilt down"' >> ~/.bashrc
+grep -qF -- 'alias cubbit="cd ~/cubbit"' ~/.bashrc || echo 'alias cubbit="cd ~/cubbit"' >> ~/.bashrc
+grep -qF -- 'alias cubbit-tilt="cubbit && tilt -f coordinator/Tiltfile"' ~/.bashrc || echo 'alias cubbit-tilt="cubbit && tilt -f coordinator/Tiltfile"' >> ~/.bashrc
+grep -qF -- 'alias cup="cubbit-tilt up"' ~/.bashrc || echo 'alias cup="cubbit-tilt up"' >> ~/.bashrc
+grep -qF -- 'alias cdown="cubbit-tilt down"' ~/.bashrc || echo 'alias cdown="cubbit-tilt down"' >> ~/.bashrc
 
 source ~/.bashrc
