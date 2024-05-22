f86808a35e1aab3218280a3adab1daa746dcf744
DangerBlack <danger.recheng@hotmail.it>
2024-05-22 14:38:13 +0200

    feat(install): add starship config

--- terminal/install.sh
+++ terminal/install.sh
@@ -4,14 +4,12 @@ set -e
 grep -qF -- "alias ttd='tilt -f coordinator/Tiltfile down" ~/.bashrc || echo "alias ttd='tilt -f coordinator/Tiltfile down'"  >> ~/.bashrc
 grep -qF -- "alias ttu='tilt -f coordinator/Tiltfile up'" ~/.bashrc || echo "alias ttu='tilt -f coordinator/Tiltfile up'"  >> ~/.bashrc
 grep -qF -- "alias gaz='bazel run //:gazelle'" ~/.bashrc || echo "alias gaz='bazel run //:gazelle'"  >> ~/.bashrc
-
+grep -qF -- "eval "$(starship init bash)" ~/.bashrc || echo "eval $(starship init bash)" >> ~/.bashrc
 
 # Install starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -y -f
 
 mkdir -p /home/coder/.config/
 
-pwd
-cp starship.toml /home/coder/.config/starship.toml
 
 source ~/.bashrc
